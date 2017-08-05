345e62e91364ea06a59a8fdbf919f78502760086
PJC-1 <jumpzine@gmail.com>
2017-08-05 00:43:28 -0700

    added pug for templating

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
 const express = require('express');
+const path = require('path');
 
+// Init app
 const app = express();
 
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home Route
 app.get('/', function(req, res){
-    res.send('Hello world.');
+    let articles = [
+        {
+            id    : 1,
+            title : 'Article One',
+            author: 'Phill',
+            body  : 'This is article one'
+        },
+        {
+            id    : 2,
+            title : 'Article Two',
+            author: 'Jordane',
+            body  : 'This is article two'
+        },
+        {
+            id    : 3,
+            title : 'Article Three',
+            author: 'Sherry',
+            body  : 'This is article three'
+        }
+    ];
+
+    res.render('index', {
+        title: 'Articles',
+        articles: articles
+    });
 });
 
+// Add Route
+app.get('/articles/add', function(req, res){
+    res.render('add_article', {
+        title: 'Add Article'
+    });
+});
+
+// Start Server
 app.listen(3000, function(){
     console.log('You are listening on port 3000...');
 });
