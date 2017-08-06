f2e723ee8ada15a644cc6f8fa9fda07d27b6f140
PJC-1 <jumpzine@gmail.com>
2017-08-06 00:41:50 -0700

    added the scripts bower and nav bar

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -25,6 +26,14 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false}));
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Home Route
 app.get('/', function(req, res){
     Article.find({}, function(err, articles){
@@ -45,6 +54,23 @@ app.get('/articles/add', function(req, res){
         title: 'Add Article'
     });
 });
+
+// Add Submit POST Route
+app.post('/articles/add', function(req, res){
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+    article.save(function(err){
+        if(err){
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    })
+});
+
 
 // Start Server
 app.listen(3000, function(){
