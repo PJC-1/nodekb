42e12e6e5a8a887c24c632340ecc010ed8eae6f2
PJC-1 <jumpzine@gmail.com>
2017-08-07 21:55:31 -0700

    added validation to the add articles functionality

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -33,6 +36,39 @@ app.use(bodyParser.json());
 
 // Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+    // cookie: { secure: true }
+}));
+
+// Express Message Middleware
+app.use(require('connect-flash')());
+app.use(function(req, res, next){
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+            var namespace = param.split('.')
+            , root = namespace.shift()
+            , formParam = root;
+        while(namespace.length){
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
+
+    }
+}));
 
 // Home Route
 app.get('/', function(req, res){
@@ -75,6 +111,7 @@ app.post('/articles/add', function(req, res){
             console.log(err);
             return;
         } else {
+            req.flash('success','Article Added');
             res.redirect('/');
         }
     })
