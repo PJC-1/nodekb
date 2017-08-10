4cad36df2d1d88d3915bfdec010fc11b58916b33
PJC-1 <jumpzine@gmail.com>
2017-08-09 22:36:11 -0700

    completed log in functionality and layout

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 // Bring in User Model
 let User = require('../models/user');
@@ -58,8 +59,19 @@ router.post('/register', function(req, res){
     }
 });
 
+
+// Login Form
 router.get('/login', function(req, res){
     res.render('login');
+});
+
+// Login Process
+router.post('/login', function(req, res, next){
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next);
 });
 
 module.exports = router;
