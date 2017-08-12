ef28752aa7745c35126ef96c4621c142e598c7bd
PJC-1 <jumpzine@gmail.com>
2017-08-12 16:56:17 -0700

    connecting to mongolab for heroku

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const passport = require('passport');
 const config = require('./config/database');
 
-mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || config.database);
+mongoose.connect(process.env.MONGOLAB_URI || config.database);
 let db = mongoose.connection;
 
 // Check connection
